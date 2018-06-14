b76b0a6c097159edce7ee22d340cc3b206234ac5
YUMEE <yumeehk@hotmail.com>
2018-06-14 15:27:39 -0400

    new method that doesnt allow more than 3 cards

--- cards.rb
+++ cards.rb
@@ -1,14 +1,19 @@
 def select_cards(possible_cards, hand)
-
+  total = 0
   possible_cards.each do |current_card|
     print "Do you want to pick up #{current_card}?"
     answer = gets.chomp
     if answer.downcase == 'y'
-      hand << current_card
+      if total < 3
+        total += 1
+        hand << current_card
+      else
+        puts "You can't have more than 3 cards."
+        exit
+      end
     end
   end
   return hand
-
 end
 
 available_cards = ['queen of spades', '2 of clubs', '3 of diamonds', 'jack of spades', 'queen of hearts']
